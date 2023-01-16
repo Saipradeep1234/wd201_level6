ba3d8f51b8382550217f2ee0f590ac91a5259614
Saipradeep1234 <113632887+Saipradeep1234@users.noreply.github.com>
2023-01-16 18:36:48 +0530

    Level 6

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const app = require("./app");
 const port = typeof process.env.PORT === "undefined" ? 3000 : process.env.PORT;
-
 app.listen(port, () => {
   console.log("Started express server at port " + port);
 });
